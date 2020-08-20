e965a673d9b11ad71a4b23c17242377942f19b37
Adi Nugroho <hidrodixtion@gmail.com>
2020-08-20 22:47:01 +0700

    Change the pod name

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,10 +9,10 @@ dockerpath="hidrodixtion/udacitydevops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacitydevops
+kubectl run udacitydevopsapi
     --generator=run-pod/v1\
     --image=$dockerpath
-    --port=80 --labels app=udacitydevops
+    --port=80 --labels app=udacitydevopsapi
 
 # Step 3:
 # List kubernetes pods
@@ -20,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward udacitydevops 8080
+kubectl port-forward udacitydevopsapi 8080
