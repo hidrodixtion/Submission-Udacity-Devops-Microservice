f212ace1cbdff9b0a20f78f12ef08452e0771777
Adi Nugroho <hidrodixtion@gmail.com>
2020-08-20 21:22:06 +0700

    Edit run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="hidrodixtion/udacitydevops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitydevops
+    --generator=run-pod/v1\
+    --image=$dockerpath
+    --port=80 --labels app=udacitydevops
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitydevops 8080
