292724f2ac01f246e5d2dc74af7de471117a1964
Adi Nugroho <hidrodixtion@gmail.com>
2020-08-20 19:32:47 +0700

    Update upload_docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="hidrodixtion/udacitydevops"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag udacitydevops $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
